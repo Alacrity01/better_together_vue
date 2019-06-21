13a29d993389a0118e69823223fd25c4526dd58a
TheAlacrity <brinker.jeff@gmail.com>
2019-06-21 16:35:01 -0500

    corrected file name to UsersShow.vue

--- src/router.js
+++ src/router.js
@@ -1,23 +1,30 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import UsersIndex from './views/UsersIndex.vue';
+import UsersNew from './views/UsersNew.vue';
+import UsersShow from './views/UsersShow.vue';
+import UsersEdit from './views/UsersEdit.vue';
+
+// import Signup from "./views/Signup.vue";
+// import Login from "./views/Login.vue";
+// import Logout from "./views/Logout.vue";
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
+  hash: false,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    {path: '/', name: 'home', component: UsersIndex },
+
+    {path: '/users', name: 'users-index', component: UsersIndex },
+    {path: '/users/new', name: 'users-new', component: UsersNew },
+    {path: '/users/:id', name: 'users-show', component: UsersShow },
+    {path: '/users/:id/edit', name: 'users-edit', component: UsersEdit },
+
+    // { path: "/signup", name: "signup", component: Signup },
+    // { path: "/login", name: "login", component: Login },
+    // { path: "/logout", name: "logout", component: Logout }
   ]
 })
